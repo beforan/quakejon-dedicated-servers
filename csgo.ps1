4c5a7519272bc0471038304ede2c7d0b2e65f3cf
Jonathan Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2015-04-02 16:33:09 +0100

    started work on config usage in csgo.ps1 only

--- csgo.ps1
+++ csgo.ps1
@@ -1,9 +1,51 @@
-﻿# Change to SteamCMD install directory
-cd c:\steamcmd
+﻿# Load config
+$config = get-content -raw config.json | ConvertFrom-Json
 
-# Update / Install the Server
-./steamcmd +runscript scripts\csgo.txt
+$server = "csgo" # internal identifier for the server (SPOT)
+
+
+# Do some paths work
+$scriptspath = ResolveConfigPaths $config.paths.root $config.paths.scripts
+$serverspath = ResolveConfigPaths $config.paths.root $config.paths.servers
+$steamcmdpath = ResolveConfigPaths $config.paths.root $config.steamcmd.exepath
+
+# change to the scripts path to prep a temporary SteamCMD script
+cd $scriptspath
+
+# prep the SteamCMD script
+try {
+  new-item "steamcmd.txt" -ItemType "file"
+}
+catch [System.IOException] {
+  clear-content "steamcmd.txt"
+}
+finally {
+  $steamscript = get-content "csgo.txt"
+  foreach ($line in $steamscript) {
+    # Make substitutions from config
+    $result = $line -replace "<username>", $config.steamcmd.login.user
+    $result = $result -replace "<password>", $config.steamcmd.login.pass
+    $result = $result -replace "<installpath>" $serverspath
+    
+    # append the variable substituted line to the new temp script
+    $result >> steamcmd.txt
+  }
+}
+
+# Update / Install the Server using the new temp script
+& (join-path $steamcmdpath "steamcmd") +runscript scripts\csgo.txt # RESUME FROM HERE!
 
 # Run the server with defaults
 cd servers/csgo
 ./srcds -console -fork -usercon -game csgo +map de_dust2 +sv_lan 1 +rcon_password qj123
+
+
+# Helper functions
+function ResolveConfigPaths {
+  param{[string]$root, [string]$target}
+  if ([System.IO.Path::isPathRooted($target)]) {
+    $target
+  } else {
+    join-path $root $target
+  }
+}
